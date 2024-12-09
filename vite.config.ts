e065586fdc13a56ea837e06142436ee574ac3537
Dmitry Nischeta <dnishcheta@gmail.com>
2024-12-09 10:14:15 +0500

    .

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vue-tma-issue',
+  base: '/vue-tma-issue/',
   plugins: [
     vue(),
     vueDevTools(),
